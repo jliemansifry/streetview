845fad99c0ccc16dbc14dd4f243592e731dbd8bd
Jesse Lieman-Sifry <jliemansifry@wesleyan.edu>
2015-11-30 23:32:05 -0700

    Tranform road coordinates for compatibility

--- coloradoGIS.py
+++ coloradoGIS.py
@@ -8,20 +8,17 @@ import shapely
 from mpl_toolkits.basemap import Basemap
 import shapefile
 from matplotlib.patches import Polygon, PathPatch
-from matplotlib.collections import PatchCollection
+from matplotlib.collections import PatchCollection, LineCollection
 import random
 # import shapely.geometry as sg
 
-num_colors = 29 # random colors for testing purposes
-cm = plt.get_cmap('Blues')
-blues = [cm(1.*i/num_colors) for i in range(num_colors)]
-
-fig = plt.figure(figsize=(20,10))
-ax = fig.add_subplot(111, axisbg='w', frame_on=False)
-
-m = Basemap(width=800000,height=550000, resolution='l',projection='aea', lat_1=37.,lat_2=41,lon_0=-105.55,lat_0=39)
-
 def plot_shapefile(f, options = 'counties'):
+    num_colors = 29 # 29 unique rock types
+    cm = plt.get_cmap('Blues')
+    blues = [cm(1.*i/num_colors) for i in range(num_colors)]
+    fig = plt.figure(figsize=(20,10))
+    ax = fig.add_subplot(111, axisbg='w', frame_on=False)
+    m = Basemap(width=800000,height=550000, resolution='l',projection='aea', lat_1=37.,lat_2=41,lon_0=-105.55,lat_0=39)
     m.readshapefile(f, name = 'state') # counties
     if options == 'geo':
         rocktypes = np.unique(np.array([m.state_info[i]['ROCKTYPE1'] for i in range(len(m.state_info))]))
@@ -35,6 +32,9 @@ def plot_shapefile(f, options = 'counties'):
             idx = np.where(rocktypes == rocktype)[0][0]
             patches = [Polygon(np.array(shape), True)]
             pc = PatchCollection(patches, edgecolor=None, facecolor = blues[idx], linewidths=.5, zorder=2)
+        if options == 'nofill':
+            patches = [Polygon(np.array(shape), True)]
+            pc = PatchCollection(patches, edgecolor='k', hatch = None, linewidths=0.5, zorder=2)
         else:
             patches = [Polygon(np.array(shape), True)]
             pc = PatchCollection(patches, edgecolor='k', linewidths=5., zorder=2)
@@ -49,20 +49,22 @@ def convert_highways(h_shp):
     orig = Proj(sh.crs)
     dest = Proj(init = 'EPSG:4326')
     with fiona.open('Highways/SHP/STATEWIDE/HIGHWAYS_corr.shp', 'w', 'ESRI Shapefile', sh.schema.copy(), crs = from_epsg(4326)) as output:
-        for feat in sh: # feat = one polygon of the shapefile
-            out_linearRing = [] # empty list for the LinearRing of transformed coordinates
-            for point in feat['geometry']['coordinates']: # LinearRing of the Polygon
-                print point
-                lng,lat =  point  # one point of the LinearRing
-                x,y = transform(orig, dest,lng,lat) # transform the point
-                print x,y 
-                out_linearRing.append((x,y)) # add all the points to the new LinearRing
-            # transform the resulting LinearRing to  a Polygon and write it
-            feat['geometry']['coordinates'] = [out_linearRing]
-            output.write(feat)
+        for feat in sh: 
+            points = feat['geometry']['coordinates']
+            lat = [point[1] for point in points]
+            lng = [point[0] for point in points]
+            if any(isinstance(sublist, list) for sublist in points):
+                flattened = [pair for combo in points for pair in combo]
+                lat = [point[1] for point in flattened]
+                lng = [point[0] for point in flattened]
+            x, y = transform(orig, dest, lng, lat) 
+            out_points = [(ln, la) for ln, la in zip(x, y)]
+            feat['geometry']['coordinates'] = out_points
+            if not any(isinstance(sublist, list) for sublist in points):
+                output.write(feat)
 
 # plot_shapefile('Shape/GU_CountyOrEquivalent')
-# plot_shapefile('Highways/SHP/STATEWIDE/HIGHWAYS')
+plot_shapefile('Highways/SHP/STATEWIDE/HIGHWAYS_corr', options = 'nofill')
 # plot_shapefile('COgeol_dd/cogeol_dd_polygon', options = 'geo')
 
 
