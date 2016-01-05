3500695bead862cf3255a2a0bff5959525b91b6c
Jesse Lieman-Sifry <jliemansifry@wesleyan.edu>
2016-01-05 11:44:52 -0700

    Remove cruft

--- imagePresentationFunctions.py
+++ imagePresentationFunctions.py
@@ -1,25 +1,15 @@
 import matplotlib
-import sys
 import numpy as np
 
 
-def make_cmap(colors, position=None, bit=False):
+def make_cmap(colors):
     '''
     INPUT:  (1) list of RGB tuples to make the colormap from
     OUTPUT: (1) matplotlib colormap
     '''
-    bit_rgb = np.linspace(0, 1, 256)
-    if position is None:
-        position = np.linspace(0, 1, len(colors))
-    else:
-        if len(position) != len(colors):
-            sys.exit("position length must be the same as colors")
-        elif position[0] != 0 or position[-1] != 1:
-            sys.exit('position must start with 0 and end with 1')
-    if bit:
-        for i in range(len(colors)):
-            colors[i] = (bit_rgb[colors[i][0]],
-                         bit_rgb[colors[i][1]], bit_rgb[colors[i][2]])
+    position = np.linspace(0, 1, len(colors))
+    colors = [(color[0] / 255., color[1] / 255., color[2] / 255.)
+              for color in colors]
     cdict = {'red': [], 'green': [], 'blue': []}
     for pos, color in zip(position, colors):
         cdict['red'].append((pos, color[0], color[0]))
@@ -43,5 +33,5 @@ def make_cmyk_greyscale_continuous_cmap():
               (192, 244, 204), (155, 255, 145), (210, 200, 12),
               (230, 180, 7), (236, 124, 13), (233, 100, 25),
               (228, 30, 45), (198, 0, 46), (103, 0, 51)]
-    cont_cmap = make_cmap(colors, bit=True)
+    cont_cmap = make_cmap(colors)
     return cont_cmap
